7034fb040e0a94cbe1b557e07489d01dbe550903
Sirson Sharma Chapagain <sirsonsharma39@gmail.com>
2026-01-01 00:54:58 +0545

    Remove Resume_system folder

--- Resume_system/main/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import ResumeUpload
-
-class ResumeUploadForm(forms.ModelForm):
-    class Meta:
-        model = ResumeUpload
-        fields = ['resume_file']
--- Resume_system/screening/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile, UploadedResume, JobDescription, ScreeningResult
-
-admin.site.register(UserProfile)
-admin.site.register(UploadedResume)
-admin.site.register(JobDescription)
-admin.site.register(ScreeningResult)
